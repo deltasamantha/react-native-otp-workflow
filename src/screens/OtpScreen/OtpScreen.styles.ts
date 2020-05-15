20a9eea912bb4e2d43bd756f925d2fb2d0073a6b
Samantha Withanage <samantha@frostdigital.se>
2020-05-15 16:48:43 +0530

    Added animation to OTP screen

--- src/screens/OtpScreen/OtpScreen.styles.ts
+++ src/screens/OtpScreen/OtpScreen.styles.ts
@@ -1,9 +1,26 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+import {colorAccent} from '../../theme/Colors';
 
 export const styles = StyleSheet.create({
     rootContainer: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+});
+
+const VIEW_WIDTH = Dimensions.get('window').width - 80;
+export const otpStyles = StyleSheet.create({
+    rootView: {
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        flex: 1,
+        fontSize: 34,
+        width: VIEW_WIDTH,
+        borderColor: colorAccent,
+        borderWidth: 1,
     },
 });
--- src/screens/OtpScreen/OtpScreen.tsx
+++ src/screens/OtpScreen/OtpScreen.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import {styles} from './OtpScreen.styles';
+import React, {useRef, useEffect} from 'react';
+import {View, Dimensions, StyleSheet} from 'react-native';
+import {styles, otpStyles} from './OtpScreen.styles';
+import Animated, {
+    Value,
+    Easing,
+    interpolate,
+    useCode,
+    cond,
+    eq,
+    set,
+} from 'react-native-reanimated';
+import {withTimingTransition} from 'react-native-redash';
+import {TextInput} from 'react-native-gesture-handler';
+import {colorAccent} from '../../theme/Colors';
 
 export default function OtpScreen() {
     return (
         <View style={styles.rootContainer}>
-            <Text>OTP</Text>
+            <OTPInput />
         </View>
+    );
+}
+
+function OTPInput() {
+    const ANIMATION_DURATION = 500;
+    const slideIn = useRef(new Value(0));
+    const slideInAnimation = withTimingTransition(slideIn.current, {
+        duration: ANIMATION_DURATION,
+        easing: Easing.ease,
+    });
+
+    const SCREEN_WIDTH = Dimensions.get('window').width;
+
+    const slideInTranslateX = interpolate(slideInAnimation, {
+        inputRange: [0, 1],
+        outputRange: [-SCREEN_WIDTH, 0],
+    });
+
+    useCode(() => cond(eq(slideIn.current, 0), set(slideIn.current, 1)), []);
+
+    return (
+        <Animated.View style={[otpStyles.rootView, {transform: [{translateX: slideInTranslateX}]}]}>
+            <TextInput
+                style={otpStyles.input}
+                keyboardType="number-pad"
+                maxLength={5}
+                returnKeyType="done"
+            />
+        </Animated.View>
     );
 }
